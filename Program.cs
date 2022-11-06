50352020185d9cb424c1b681316d08e211c071fe
Mohammad Oudetalla <moudetalla@asaltech.com>
2022-11-06 01:01:44 -0700

    this is my first commit for pre-task1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person("Mohammad");
+            person.Greet();
+
+
+
+            var student = new Student("Ahmad");
+            student.SetAge(18);
+            student.Greet();
+            student.ShowAge();
+
+
+            var prof = new Professor("Saleh");
+            prof.SetAge(48);
+            prof.Greet();
+
+            prof.Explain();
+
+
+
+
+
+        }
+    }
+}
